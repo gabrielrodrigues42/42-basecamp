01f826f37829a8508513974a777a458bdc000f7e
gandrade <gandrade@student.42sp.org.br>
2021-04-15 13:34:22 +0000

    Basecamp C 01 (succeeded with 85%)

---
+++ basecamp_c_01/ex05/ft_putstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gandrade <gandrade@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/07 16:52:30 by gandrade          #+#    #+#             */
+/*   Updated: 2021/04/08 15:53:56 by gandrade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, sizeof(c));
+}
